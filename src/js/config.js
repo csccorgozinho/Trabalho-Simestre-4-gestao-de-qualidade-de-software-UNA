3db4017342a362d9597a850bc255d71f4c715f0f
caio silva campos corgozinho <104537051+csccorgozinho@users.noreply.github.com>
2023-10-23 16:46:13 -0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5ddc1a46-3b65-4f8d-b6b5-e81f9d9e3189';
+export const MODAL_CLOSE_SEC = 2.5;
